5097a1e09edca23ec0b23a4a33b888098fbe6a96
Satyam-droid <25satyam25@gmail.com>
2022-01-07 14:54:54 +0530

    sec commit

--- Email_automation/settings.py
+++ Email_automation/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 import environ
 
@@ -25,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%)%9o0i@k4r-d)2o_%^)4e#!@8&bom0l_nlsl!xpmpb$x_*g(u'
+SECRET_KEY =os.environ['SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["email-automation-app.herokuapp.com","localhost"]
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT Email_automation.wsgi:application
